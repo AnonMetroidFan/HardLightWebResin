cc4e0e6f7937eb49c47669de8a905866693ff5c1
Brinstar77 <grntbrght@gmail.com>
2026-03-13 16:09:28 -0400

    * Vacbed C#

--- Content.Shared/_HL/Vacbed/SharedInsideVacbedSystem.cs
+++ Content.Shared/_HL/Vacbed/SharedInsideVacbedSystem.cs
@@ -1,5 +1,20 @@
 using Content.Shared.Eye.Blinding.Systems;
 using Content.Shared.Standing;
+using Content.Shared.ActionBlocker;
+using Content.Shared.Database;
+using Content.Shared.Emoting;
+using Content.Shared.Hands;
+using Content.Shared.Interaction;
+using Content.Shared.Interaction.Events;
+using Content.Shared.Inventory.Events;
+using Content.Shared.Item;
+using Content.Shared.Movement.Events;
+using Content.Shared.Pulling.Events;
+using Content.Shared.Speech;
+using Content.Shared.Standing;
+using Content.Shared.StatusEffect;
+using Content.Shared.Storage.Components;
+using Content.Shared.Throwing;
 using Robust.Shared.Containers;
 
 namespace Content.Shared._HL.Vacbed;
@@ -7,6 +22,7 @@ namespace Content.Shared._HL.Vacbed;
 public abstract partial class SharedVacbedSystem
 {
     [Dependency] private readonly BlindableSystem _blindableSystem = default!;
+    [Dependency] private readonly ActionBlockerSystem _blocker = default!;
 
     public virtual void InitializeInsideVacbed()
     {
@@ -14,6 +30,18 @@ public abstract partial class SharedVacbedSystem
         SubscribeLocalEvent<InsideVacbedComponent, EntGotRemovedFromContainerMessage>(OnEntGotRemovedFromContainer);
         SubscribeLocalEvent<InsideVacbedComponent, ComponentInit>(InsideVacbedInit);
         SubscribeLocalEvent<InsideVacbedComponent, CanSeeAttemptEvent>(OnVacbedTrySee);
+        SubscribeLocalEvent<InsideVacbedComponent, ChangeDirectionAttemptEvent>(OnAttempt);
+        SubscribeLocalEvent<InsideVacbedComponent, StandAttemptEvent>(OnAttempt);
+        SubscribeLocalEvent<InsideVacbedComponent, UseAttemptEvent>(OnAttempt);
+        SubscribeLocalEvent<InsideVacbedComponent, ThrowAttemptEvent>(OnAttempt);
+        SubscribeLocalEvent<InsideVacbedComponent, DropAttemptEvent>(OnAttempt);
+        SubscribeLocalEvent<InsideVacbedComponent, AttackAttemptEvent>(OnAttempt);
+        SubscribeLocalEvent<InsideVacbedComponent, PickupAttemptEvent>(OnAttempt);
+        SubscribeLocalEvent<InsideVacbedComponent, IsEquippingAttemptEvent>(OnAttempt);
+        SubscribeLocalEvent<InsideVacbedComponent, IsUnequippingAttemptEvent>(OnAttempt);
+        SubscribeLocalEvent<InsideVacbedComponent, StartPullAttemptEvent>(OnAttempt);
+        SubscribeLocalEvent<InsideVacbedComponent, EmoteAttemptEvent>(OnAttempt);
+        SubscribeLocalEvent<InsideVacbedComponent, InteractionAttemptEvent>(OnInteractAttempt);
     }
 
     private void HandleDown(EntityUid uid, InsideVacbedComponent component, DownAttemptEvent args)
@@ -42,6 +70,22 @@ public abstract partial class SharedVacbedSystem
 
         RemComp<InsideVacbedComponent>(uid);
         _blindableSystem.UpdateIsBlind(component.Owner);
+    }
+
+    /// <summary>
+    /// Prevents any attempt by the player to do anything
+    /// </summary>
+    private void OnAttempt(EntityUid uid, InsideVacbedComponent component, CancellableEntityEventArgs args)
+    {
+        args.Cancel();
+    }
+
+    /// <summary>
+    /// Prevents any player attempt to interact with world
+    /// </summary>
+    private void OnInteractAttempt(EntityUid uid, InsideVacbedComponent component, ref InteractionAttemptEvent args)
+    {
+        args.Cancelled = true;
     }
 
 }
